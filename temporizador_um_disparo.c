1ea41fb018cf2c545ed3febc87f678a0e29c062d
bigodinhojf <jfelipe.teles1@gmail.com>
2025-02-04 13:29:31 -0300

    Parte do código principal pronto

--- temporizador_um_disparo.c
+++ temporizador_um_disparo.c
@@ -1,24 +1,71 @@
+// Inclusão das bibliotecas utilizadas
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/timer.h"
+#include "hardware/timer.h" // Biblioteca de temporizadores
 
+// Definição dos pinos GPIO
+const int led_pin_green = 11; // LED Verde GPIO 11
+const int led_pin_blue = 12; // LED azul GPIO 12
+const int led_pin_red = 13; // LED vermelho GPIO 13
+const int button_pin_a = 5; // Botão A GPIO 5
+
+// Variáveis globais
+bool activate = false; // Armazena a informação se a rotina está rodando
+static volatile uint32_t last_time = 0; // Armazena o tempo do último clique dos botões
+
+// Função de callback do alarm
 int64_t alarm_callback(alarm_id_t id, void *user_data) {
     // Put your timeout handler code in here
     return 0;
 }
 
 
-
+// Função principal
 int main()
 {
+    // Inicialização da comunicação serial
     stdio_init_all();
 
-    // Timer example code - This example fires off the callback after 2000ms
-    add_alarm_in_ms(2000, alarm_callback, NULL, false);
-    // For more examples of timer use see https://github.com/raspberrypi/pico-examples/tree/master/timer
+    // Inicialização dos pinos GPIO
+
+    // Inicializa o LED Verde GPIO 11 e o define como saída
+    gpio_init(led_pin_green);
+    gpio_set_dir(led_pin_green, GPIO_OUT);
+
+    // Inicializa o LED Azul GPIO 12 e o define como saída
+    gpio_init(led_pin_blue);
+    gpio_set_dir(led_pin_blue, GPIO_OUT);
+
+    // Inicializa o LED Vermelho GPIO 13 e o define como saída
+    gpio_init(led_pin_red);
+    gpio_set_dir(led_pin_red, GPIO_OUT);
+
+    // Inicializa o Botão A, o define como entrada e habilita o resistor de pull up
+    gpio_init(button_pin_a);
+    gpio_set_dir(button_pin_a, GPIO_IN);
+    gpio_pull_up(button_pin_a);
+
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // Verifica se o botão foi pressionado e se não há uma rotina rodando
+        if(gpio_get(button_pin_a) == 0){
+            // Debouncing
+            uint32_t current_time = to_us_since_boot(get_absolute_time()); // Pega o tempo atual e transforma em us
+            // 200 ms
+            if(current_time - last_time > 200000 && !activate){
+                last_time = current_time; // Atualização de tempo do último clique
+
+                // Define a rotina como 'rodando'
+                activate = true;
+
+                // Liga todos os LEDs
+                gpio_put(led_pin_green, 1);
+                gpio_put(led_pin_blue, 1);
+                gpio_put(led_pin_red, 1);
+
+                // Define um alarme para desligar os LEDs a cada 3s
+                add_alarm_in_ms(3000, alarm_callback, NULL, false);
+            }
+        }
     }
 }
